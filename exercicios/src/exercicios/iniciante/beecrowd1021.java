18b5f848600f2f899281dfd99aeed9b782c480b8
Luis Felipe <otaldolu.isborges4522@outlook.com.br>
2024-01-23 17:34:07 -0400

    ---

--- exercicios/src/exercicios/iniciante/beecrowd1021.java
+++ exercicios/src/exercicios/iniciante/beecrowd1021.java
@@ -9,35 +9,63 @@ public class beecrowd1021 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double N = sc.nextDouble();
-        //int V = 0;
+        double N =  sc.nextDouble();
+        int i = 0;
+        
         System.out.println("NOTAS:");
-        System.out.println((int) N / 100 + " nota(s) de R$ 100.00");
-        N = N % 100;
-        System.out.println((int) N / 50 + " nota(s) de R$ 50.00");
-        N = N % 50;
-        System.out.println((int) N / 20 + " nota(s) de R$ 20.00");
-        N = N % 20;
-        System.out.println((int) N / 10 + " notas(s) de R$ 10.00");
-        N = N % 10;
-        System.out.println((int) N / 5 + " notas(s) de R$ 5.00");
-        N = N % 5;
-        System.out.println((int) N / 2 + " notas(s) de R$ 2.00");
-        N = N % 2;
+        
+        i = (int) N / 100;
+        System.out.printf("%d nota(s) de R$ 100.00%n", i);
+        N = N % 100.0;
+        
+        i = (int) N / 50;
+        System.out.printf("%d nota(s) de R$ 50.00%n", i);
+        N = N % 50.0;
+        
+        i = (int) N / 20;
+        System.out.printf("%d nota(s) de R$ 20.00%n", i);
+        N = N % 20.0;
+        
+        i = (int) N / 10;
+        System.out.printf("%d nota(s) de R$ 10.00%n", i);
+        N = N % 10.0;
+        
+        i = (int) N / 5;
+        System.out.printf("%d nota(s) de R$ 5.00%n", i);
+        N = N % 5.0;
+        
+        i = (int) N / 2;
+        System.out.printf("%d nota(s) de R$ 2.00%n", i);
+        N = N % 2.0;
 
+        N = N * 100;
         System.out.println("MOEDAS:");
-        System.out.println((int)N / 1.00 + " moeda(s) de R$ 1.00");
-        N = N % 1.00;
-        System.out.println(N / 0.50 + " moeda(s) de R$ 0.50");
-        N = N % 0.50;
-        System.out.println(N / 0.25 + " moeda(s) de R$ 0.25");
-        N = N % 0.25;
-        System.out.println(N / 0.10 + " moeda(s) de R$ 0.10");
-        N = N % 0.10;
-        System.out.println(N / 0.05 + " moeda(s) de R$ 0.05");
-        N = N % 0.05;
-        System.out.println(N / 0.01 + " moeda(s) de R$ 0.01");
-        N = N % 0.01;
+        
+        i = (int) N / 100;
+        System.out.printf("%d moeda(s) de R$ 1.00%n", i);
+        N = N % 100.0;
+        
+        i = (int) N / 50;
+        System.out.printf("%d moeda(s) de R$ 0.50%n", i);
+        N = N % 50.0;
+        
+        i = (int)N / 25;
+        System.out.printf("%d moeda(s) de R$ 0.25%n", i);
+        N = N % 25.0;
+        
+        i = (int) N / 10;
+        System.out.printf("%d moeda(s) de R$ 0.10%n", i);
+        N = N % 10.0;
+        
+        i = (int) N / 5;
+        System.out.printf("%d moeda(s) de R$ 0.05%n", i);
+        N = N % 5.0;
+        
+        i = (int) N / 1;
+        System.out.printf("%d moeda(s) de R$ 0.01%n", i);
+        N = N % 1.0;
+
         sc.close();
     }
 }
+// Accepted
