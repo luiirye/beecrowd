b8b9989ba2db4a292841680d5d92aa478f562314
luiirye <borg.esluii01645@gmail.com>
2026-09-04 17:30:22 -0400

    Exercício 1071 incompleto

---
+++ iniciante/python/bee1071.py
@@ -0,0 +1,12 @@
+class Soma:
+    def __init__(self):
+        
+        # decração de atributos da classe
+        
+        self.a:int
+        self.b:int
+        
+    def soma(self, a, b) -> int: # Esse método retorna um valor inteiro
+        return a + b
+        
+    
